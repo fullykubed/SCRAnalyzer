d16ebd28ad940b1b20190def5d7d5ed9f2514f6a
Jack Langston <jclangst@indiana.edu>
2018-07-26 17:14:12 -0400

    Removed debugging print statement

--- SCRAnalyzer.py
+++ SCRAnalyzer.py
@@ -63,7 +63,6 @@ if __name__ == "__main__":
     #get the number of measurements for each minute
     minute_number_for_each_row = (RAW_DATA[:, 0] / 60).astype(np.int)
     measures_per_min = np.histogram(minute_number_for_each_row, np.arange(0, np.max(minute_number_for_each_row) + 1))[0]
-    print(np.histogram(minute_number_for_each_row, np.arange(0, np.max(minute_number_for_each_row) + 2)))
 
     #Create the Summary Statistics
     output = [
